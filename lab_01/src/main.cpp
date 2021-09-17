7973d823d837bc2f1d2a30b20f7adc9729a89e36
Ekaterina_Varlamova <katy1781@inbox.ru>
2021-09-17 13:36:20 +0300

    report fixed

--- lab_01/src/main.cpp
+++ lab_01/src/main.cpp
@@ -22,16 +22,17 @@ double checkTime(Array<Type> &a, int (*cmp)(Type, Type), void (Array<Type>::*sor
     for (int i = 0; i < 100; i++) {
         for (size_t i = 0; i < a.getLength(); i++)
             a[i] = rand() % 1000 - 500;
-        gettimeofday(&tv_start, NULL);
+        clock_t sTime = clock();
         (a.*sort)(cmp);
-        gettimeofday(&tv_stop, NULL);
-        time += (tv_stop.tv_sec - tv_start.tv_sec) * 1000000LL + (tv_stop.tv_usec - tv_start.tv_usec);
+        clock_t eTime = clock();
+        time += (eTime - sTime);
     }
     time /= 100.0;
     return time;
 }
 int main() {
-    for (int size = 10; size < 1000; size += 100) {
+    // latex
+    for (int size = 100; size < 1100; size += 100) {
         printf("%d &", size);
         Array<int> a(size, 0);
         printf(" %.2lf &", checkTime(a, cmp, &Array<int>::bubble));
@@ -40,5 +41,45 @@ int main() {
         printf("\n");
     }
 
+    // python
+//    printf("[");
+//    for (int size = 100; size < 1100; size += 100) {
+//        Array<int> a(size, 0);
+//        if (size != 1000)
+//            printf("%d, ", size);
+//        else
+//            printf("%d", size);
+//    }
+//    printf("]\n");
+//
+//    printf("[");
+//    for (int size = 100; size < 1100; size += 100) {
+//        Array<int> a(size, 0);
+//        if (size != 1000)
+//            printf("%.2lf, ", checkTime(a, cmp, &Array<int>::bubble));
+//        else
+//            printf("%.2lf", checkTime(a, cmp, &Array<int>::bubble));
+//    }
+//    printf("]\n");
+//
+//    printf("[");
+//    for (int size = 100; size < 1100; size += 100) {
+//        Array<int> a(size, 0);
+//        if (size != 1000)
+//            printf("%.2lf, ", checkTime(a, cmp, &Array<int>::selection));
+//        else
+//            printf("%.2lf", checkTime(a, cmp, &Array<int>::selection));
+//    }
+//    printf("]\n");
+//
+//    printf("[");
+//    for (int size = 100; size < 1100; size += 100) {
+//        Array<int> a(size, 0);
+//        if (size != 1000)
+//            printf("%.2lf, ", checkTime(a, cmp, &Array<int>::qsort));
+//        else
+//            printf("%.2lf", checkTime(a, cmp, &Array<int>::qsort));
+//    }
+//    printf("]\n");
     return 0;
 }
