065137948d2b00959f68885b7daa45f0b9c0ad5c
Ekaterina_Varlamova <katy1781@inbox.ru>
2021-09-17 17:56:36 +0300

    ready

--- lab_01/src/main.cpp
+++ lab_01/src/main.cpp
@@ -21,25 +21,33 @@ double checkTime(Array<Type> &a, int (*cmp)(Type, Type), void (Array<Type>::*sor
     double time = 0;
     for (int i = 0; i < 100; i++) {
         for (size_t i = 0; i < a.getLength(); i++)
-            a[i] = rand() % 1000 - 500;
+            a[i] = a.getLength() - i;
         clock_t sTime = clock();
         (a.*sort)(cmp);
         clock_t eTime = clock();
         time += (eTime - sTime);
     }
-    time /= 100.0;
+    time /= .001 * CLOCKS_PER_SEC;
     return time;
 }
 int main() {
+    int length = 10;
+    Array<int> b(length, 0);
+    for (int i = 0; i < length; i++)
+        b[i] = rand() % 10 - 5;
+    std::cout << b <<std::endl;
+    b.qsort(cmp);
+    std::cout << b;
+
     // latex
-    for (int size = 100; size < 1100; size += 100) {
-        printf("%d &", size);
-        Array<int> a(size, 0);
-        printf(" %.2lf &", checkTime(a, cmp, &Array<int>::bubble));
-        printf(" %.2lf &", checkTime(a, cmp, &Array<int>::selection));
-        printf(" %.2lf  \\\\ \n\\hline", checkTime(a, cmp, &Array<int>::qsort));
-        printf("\n");
-    }
+//    for (int size = 100; size < 1100; size += 100) {
+//        printf("%d &", size);
+//        Array<int> a(size, 0);
+//        printf(" %.2lf &", checkTime(a, cmp, &Array<int>::bubble));
+//        printf(" %.2lf &", checkTime(a, cmp, &Array<int>::selection));
+//        printf(" %.2lf  \\\\ \n\\hline", checkTime(a, cmp, &Array<int>::qsort));
+//        printf("\n");
+//    }
 
     // python
 //    printf("[");
